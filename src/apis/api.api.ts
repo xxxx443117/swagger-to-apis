4d8db1a9a7d0e6989905511828b6494d65069b89
神秘人 <aaa@g.com>
2023-09-23 12:00:24 +0800

    feat: remove other files

--- src/apis/api.api.ts
+++
@@ -1,7 +0,0 @@
-import { isSuccess } from './util';
-import { SwaggerApi } from './swagger/swagger.api';
-
-export const Api = {
-  isSuccess,
-  SwaggerApi: new SwaggerApi(),
-};
--- src/apis/type.ts
+++
@@ -1,5 +0,0 @@
-export enum ResponseCode {
-  CUSTOM_SUCCESS = 1,
-  SUCCESS = 200,
-  UNAUTHORIZED = 401,
-}
