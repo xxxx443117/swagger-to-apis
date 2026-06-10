5fff131878d640936f9656d2f8855ae5e7a8b6c5
拿破仑 <109192148+xxxx443117@users.noreply.github.com>
2026-06-10 14:49:24 +0800

    test: 更新测试脚本使用 dirName 参数

--- test/index.ts
+++ test/index.ts
@@ -3,16 +3,18 @@ import { swaggerToApis } from '../src/index';
 // import * as swagger_v3 from './datas/swagger_v3.json';
 // import { AllSwaggerDocumentVersions } from '../src/types';
 
-// swaggerToApis({
-//   // doc: swagger_v2 as AllSwaggerDocumentVersions,
-//   url: 'https://petstore3.swagger.io/api/v3/openapi.json',
-//   output: './output-v3-apis',
-// });
+swaggerToApis({
+  // doc: swagger_v2 as AllSwaggerDocumentVersions,
+  url: 'https://bird-points-go.qmuzpo.easypanel.host/api.json',
+  output: './output-v3-apis',
+  dirName: 'api-v3'
+});
 
 swaggerToApis({
   // doc: swagger_v2 as AllSwaggerDocumentVersions,
-  url: 'http://143.198.45.160:12350/swagger/doc.json',
+  url: 'https://go-mall-go.qmuzpo.easypanel.host/swagger/doc.json',
   output: './output-v2-apis',
+  dirName: 'api-v2'
 });
 
 export default {};
